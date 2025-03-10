5fa68c4948ee3470bd60889e503b9f15aa03175d
Andrew Carlson <andrewcarlson272@gmail.com>
2025-03-10 18:31:26 -0500

    accept arguments

--- proj2.c
+++ proj2.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "proj2.h"
 
-int main ( int argc, char *argv[] ) {
+int main ( int argumentCount, char *argumentValues[] ) {
 
-	acceptArguments(argc, argv);
+	acceptArguments(argumentCount, argumentValues);
 	
 	return 0;
 }
 
-void acceptArguments( int argc, char *argv[] ) {
-	if ( argc == 4 ) printf("Valid\n");
-	else printf("Invalid\n");
+void acceptArguments( int argumentCount, char *argumentValues[] ) {
+	if ( argumentCount == 4 ){
+		sequence_file = argumentValues[1];
+		subsequence_file = argumentValues[2];
+		process_num = atoi(argumentValues[3]);
+		printArguments();
+	}
+	else printf("ERROR 1: Invalid Number of Arguments\n");
 }
+
+void printArguments( void ){
+	printf("sequence_file: %s\n", sequence_file);
+	printf("subsequence_file: %s\n", subsequence_file);
+	printf("process_num: %d\n", process_num);
+}
+
--- proj2.h
+++ proj2.h
@@ -1,4 +1,10 @@
 
+int process_num;
+char *sequence_file, *subsequence_file;
+
+
+// PASSING IN ARGUMENTS
 void acceptArguments( int argc, char *argv[] );
 
 
+void printArguments( void );
